1576473b815781d56cba80f9d907681ba00298a1
nhandzai <ducnhan1522004@gmail.com>
2024-02-15 23:55:46 +0700

    format change

--- src/index.js
+++ src/index.js
@@ -16,9 +16,11 @@ const { joinVoiceChannel,
     StreamType,
     NoSubscriberBehavior,
     VoiceConnectionStatus } = require('@discordjs/voice');
-const { path,join } = require('node:path');
-const { playSong } = require('./audio-player')
+const { path, join } = require('node:path');
 
+const { playSong } = require('./audio-player');
+const { interactionHandle } = require('./interaction-handle');
+const { messageHandle } = require('./message-handle');
 
 client.on('ready', (c) => {
     console.log(`${c.user.tag} is online`);
@@ -27,68 +29,10 @@ const player = createAudioPlayer();
 
 
 client.on('interactionCreate', (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-
-    if (interaction.commandName === 'ping') {
-        interaction.reply('pong');
-    }
-    if (interaction.commandName === 'baubau') {
-        interaction.reply('bau bau bau bau bau bau bau bau')
-    }
-
-    if (interaction.commandName === 'join') {
-        const voiceChannel = interaction.options.getChannel('channel');
-
-        const connection = joinVoiceChannel({
-            channelId: voiceChannel.id,
-            guildId: interaction.guildId,
-            adapterCreator: interaction.guild.voiceAdapterCreator,
-        })
-        interaction.reply('Bau bau')
-    }
-
-    if (interaction.commandName === 'fjoin') {
-        if (!interaction.member.voice.channel) {
-            interaction.reply('You are not in a voice channel!');
-            return;
-        }
-        const voiceChannel = interaction.member.voice.channel;
-
-        const connection = joinVoiceChannel({
-            channelId: voiceChannel.id,
-            guildId: interaction.guildId,
-            adapterCreator: interaction.guild.voiceAdapterCreator,
-        });
-        interaction.reply('Bau bau')
-    }
-})
-
+    interactionHandle(interaction, player)
+});
 client.on('messageCreate', (msg) => {
-    if (msg.author.bot) return;
-
-    if (msg.content === "bau")
-        msg.reply("Bau bau");
-
-    if (msg.content === "join") {
-        if(!msg.member.voice.channel){
-            msg.reply('You are not in a voice channel!')
-            return;
-        }
-
-        const connection = joinVoiceChannel({
-            channelId: msg.member.voice.channel.id,
-            guildId: msg.guildId,
-            adapterCreator: msg.guild.voiceAdapterCreator,
-        })
-        let intro = createAudioResource(join(__dirname, '..', 'audio', process.env.INTRO));
-
-        playSong(connection,player,intro);
-        // player.play(intro);
-        // connection.subscribe(player);
-        // connection.on(VoiceConnectionStatus.Ready, () => {
-        //     console.log('The connection has entered the Ready state - ready to play audio!');
-        // });
-    }
-})
+    messageHandle(msg, player)
+});
 
 client.login(process.env.TOKEN);
